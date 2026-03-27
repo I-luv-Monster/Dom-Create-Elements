76997c2bb73b2eb9e069dc567dbaf284e3206ffb
I-luv-Monster <abrahamavalenzuelaiii@gmail.com>
2026-03-27 21:39:47 +0000

    gjhdndfghjhgdjmngfhcmcvhm

---
+++ script.js
@@ -0,0 +1,14 @@
+function start() {
+    // 1. Create a heading
+    let heading = document.createElement("h1");
+
+    // 2.  Set text and styles
+    heading.innerText = "Attendance";
+    heading.style.color = "Red";
+    heading.style.textDecoration = "bold";
+
+    // 3. Add the heading to the page
+    document.body.appendChild(heading);
+
+    addName
+}
